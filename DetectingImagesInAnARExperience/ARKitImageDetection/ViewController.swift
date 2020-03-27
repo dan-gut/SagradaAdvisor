d8a6e0d7a8dd8d3c837a8ff90b3a63771cdd2aa3
Piotr Knapczyk <piotr.knapczyk@me.com>
2020-03-27 21:49:58 +0100

    iOS project

---
+++ DetectingImagesInAnARExperience/ARKitImageDetection/ViewController.swift
@@ -0,0 +1,119 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+Main view controller for the AR experience.
+*/
+
+import ARKit
+import SceneKit
+import UIKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+    
+    @IBOutlet var sceneView: ARSCNView!
+    
+    @IBOutlet weak var blurView: UIVisualEffectView!
+    
+    /// The view controller that displays the status and "restart experience" UI.
+    lazy var statusViewController: StatusViewController = {
+        return childViewControllers.lazy.compactMap({ $0 as? StatusViewController }).first!
+    }()
+    
+    /// A serial queue for thread safety when modifying the SceneKit node graph.
+    let updateQueue = DispatchQueue(label: Bundle.main.bundleIdentifier! +
+        ".serialSceneKitQueue")
+    
+    /// Convenience accessor for the session owned by ARSCNView.
+    var session: ARSession {
+        return sceneView.session
+    }
+    
+    // MARK: - View Controller Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sceneView.delegate = self
+        sceneView.session.delegate = self
+
+        // Hook up status view controller callback(s).
+        statusViewController.restartExperienceHandler = { [unowned self] in
+            self.restartExperience()
+        }
+    }
+
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		
+		// Prevent the screen from being dimmed to avoid interuppting the AR experience.
+		UIApplication.shared.isIdleTimerDisabled = true
+
+        // Start the AR experience
+        resetTracking()
+	}
+	
+	override func viewWillDisappear(_ animated: Bool) {
+		super.viewWillDisappear(animated)
+
+        session.pause()
+	}
+
+    // MARK: - Session management (Image detection setup)
+    
+    /// Prevents restarting the session while a restart is in progress.
+    var isRestartAvailable = true
+
+    /// Creates a new AR configuration to run on the `session`.
+    /// - Tag: ARReferenceImage-Loading
+	func resetTracking() {
+        
+        guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Plansze", bundle: nil) else {
+            fatalError("Missing expected asset catalog resources.")
+        }
+        
+        let configuration = ARImageTrackingConfiguration()
+        configuration.trackingImages = referenceImages
+        session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+
+        statusViewController.scheduleMessage("Look around to detect images", inSeconds: 7.5, messageType: .contentPlacement)
+	}
+
+    // MARK: - ARSCNViewDelegate (Image detection results)
+    /// - Tag: ARImageAnchor-Visualizing
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let imageAnchor = anchor as? ARImageAnchor else { return }
+        let referenceImage = imageAnchor.referenceImage
+        updateQueue.async {
+            
+            // Create a plane to visualize the initial position of the detected image.
+            let plane = SCNPlane(width: referenceImage.physicalSize.width,
+                                 height: referenceImage.physicalSize.height)
+            plane.firstMaterial?.diffuse.contents = UIColor.red.cgColor
+            let planeNode = SCNNode(geometry: plane)
+            planeNode.opacity = 0.8
+            
+            /*
+             `SCNPlane` is vertically oriented in its local coordinate space, but
+             `ARImageAnchor` assumes the image is horizontal in its local space, so
+             rotate the plane to match.
+             */
+            planeNode.eulerAngles.x = -.pi / 2
+            
+            /*
+             Image anchors are not tracked after initial detection, so create an
+             animation that limits the duration for which the plane visualization appears.
+             */
+            
+            // Add the plane visualization to the scene.
+            node.addChildNode(planeNode)
+        }
+
+        DispatchQueue.main.async {
+            let imageName = referenceImage.name ?? ""
+            self.statusViewController.cancelAllScheduledMessages()
+            self.statusViewController.showMessage("Detected image “\(imageName)”")
+        }
+    }
+
+}
